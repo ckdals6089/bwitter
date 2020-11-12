afefe8f8f46bffcff84078a5c5270ed1e5553b1b
Changmin Shin <ckdals6089@gmail.com>
2020-11-12 12:02:36 -0500

    Updates

---
+++ precache-manifest.58acd503394c6de7c20f4b15ca959431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7e957bbf1fa3ecbd6faed077a6b78f",
+    "url": "/bwitter/index.html"
+  },
+  {
+    "revision": "5e565bc983abac9a429f",
+    "url": "/bwitter/static/css/main.72a1bfd8.chunk.css"
+  },
+  {
+    "revision": "191ff886eaa78c07a2df",
+    "url": "/bwitter/static/js/2.52d3bd27.chunk.js"
+  },
+  {
+    "revision": "83bf41dee0a06314cf24e5b90389e38b",
+    "url": "/bwitter/static/js/2.52d3bd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e565bc983abac9a429f",
+    "url": "/bwitter/static/js/main.685ad6a6.chunk.js"
+  },
+  {
+    "revision": "7c2d8d71ee3fa129f73a",
+    "url": "/bwitter/static/js/runtime-main.b824e115.js"
+  },
+  {
+    "revision": "b6fdb06d30a37ce6946eeccca2167832",
+    "url": "/bwitter/static/media/sidebar.b6fdb06d.png"
+  }
+]);
