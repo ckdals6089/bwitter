6e358971f84980ead276e6da6f3ba094fd5decb2
Changmin Shin <ckdals6089@gmail.com>
2020-11-15 22:27:37 -0500

    Add a method that guest sign in

--- src/routes/Auth/Auth.js
+++ src/routes/Auth/Auth.js
@@ -5,6 +5,7 @@ import {
     faGoogle,
     faGithub,
 } from '@fortawesome/free-brands-svg-icons';
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import { authService, firebaseInstance } from 'fbase';
 import AuthForm from 'components/Auth/AuthForm';
 
@@ -22,6 +23,21 @@ const Auth = () => {
         const data = await authService.signInWithPopup(provider);
         console.log(data);
     };
+    const onGuestClick = async (event) => {
+        authService.signInAnonymously().catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+        authService.onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+            } else {
+                // User is signed out.
+            }
+        });
+    }
     return (
         <div className="authContainer">
             <FontAwesomeIcon
@@ -38,6 +54,9 @@ const Auth = () => {
                     </button>
                     <button onClick={onSocialClick} name="github" className="authBtn">
                         <span>Continue with Github&#160;</span><FontAwesomeIcon color="white" icon={faGithub} size="2x" />
+                    </button>
+                    <button onClick={onGuestClick} name="guest" className="authBtn">
+                        <span>Sign in as Guest&#160;&#160;</span><FontAwesomeIcon color="white" icon={faUserFriends} size="2x" />
                     </button>
                 </div>
             </div>
